751dca5485b70b7cb65ce5ca139377a74c844617
AleDiazV <pellahuenco@gmail.com>
2017-07-08 16:20:05 -0400

    ejercicio1

---
+++ ejercicio1.rb
@@ -0,0 +1,56 @@
+# El archivo casino.txt contiene las recaudaciones de 4 dias de un casino
+# separadas por mesa de juego.
+# Crear una clase Table cuyo constructor reciba el nombre de la mesa y
+# las recaudaciones correspondientes de cada dia.
+
+# Crear un metodo que permita leer el archivo e instanciar una mesa
+# por linea del archivo.
+
+# Crear metodos que permitan:
+# Conocer el mayor valor recaudado, y a que mesa y dia corresponde.
+# Calcular el promedio total de lo recaudado por todas las mesas en todos
+# los dias.
+
+class Table
+
+  attr_accessor :name, :collections
+
+  def initialize(name, *collections)
+    @name = name
+    @collections = collections.map(&:to_i)
+  end
+
+  def greater_collection
+
+    a = @collections.max
+
+  end
+  def day
+      day = @collections.index(@collections.max) + 1
+  end
+
+    def average
+      @collections.inject(&:+) / @collections.size.to_f
+    end
+  end
+
+
+file = File.open('casino.txt', 'r')
+data = file.readlines
+file.close
+tables = []
+data.each do |line|
+  ls = line.split(', ')
+  tables << Table.new(*ls)
+end
+
+
+prom = 0
+tables.each do |table|
+  print "El mayor valor recaudado de la #{table.name} es "
+  print "#{table.greater_collection} el dia #{table.day}"
+  prom += table.average / tables.size
+  puts
+end
+
+puts "El promedio total de lo recaudado es #{prom}"
---
+++ ejercicio3.rb
@@ -0,0 +1,53 @@
+# Se tiene un array que contiene los numeros de una ruleta.
+
+# Crear una clase llamada Roulette.
+
+# Crear un metodo de instancia llamado play que reciba como argumento un numero
+# al cual se esta apostando. Luego debe tomar un valor -del array- al azar y
+# compararlo con el numero al cual se aposto.
+
+# Crear un metodo de instancia que:
+
+# Debe almacenar el numero generado al azar en un archivo llamado
+# roulette_history.txt
+
+# Si el numero al cual se aposto corresponde al generado al azar entonces,
+# debe ademas almacenar ese numero en un archivo llamado winners.txt
+
+# Crear un metodo que lea el archivo rouletter_history.txt y retorne el valor
+# que mas ha generado la ruleta historicamente.
+
+class Roulette
+  attr_accessor :num
+  def initialize(*num)
+    @num = num
+
+  end
+  def play(num)
+    @num.sample
+  end
+  def to_s
+    "#{@num}"
+  end
+end
+r = (1..10).to_a
+
+
+file = File.open('roulette_history.txt', 'w')
+file.puts r.sample
+file.close
+
+file = File.open('roulette_history.txt', 'r')
+data = file.read
+file.close
+
+
+roe = Roulette.new(*r)
+a = roe.play(r)
+puts a
+puts data
+if a.to_i == data.to_i
+  file = File.open('winners.txt', 'a')
+  file.puts a
+  file.close
+end
